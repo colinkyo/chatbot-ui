8c63074314bdce5e9e9073597becf2627df7af4b
7yan <79803723@qq.com>
2023-04-03 14:07:23 +0800

    20230403：更新为自己的openAI帐号了

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,4 +2,4 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://openai.api2d.net';
+  process.env.OPENAI_API_HOST || 'https://service-azbaxpok-1251140691.sg.apigw.tencentcs.com/release';
