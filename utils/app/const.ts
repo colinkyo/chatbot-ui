0cd2aaa6ee19ebbe47da8936646a68343c6ccbc1
colinkyo <79803723@qq.com>
2023-06-07 11:30:12 +0800

    20230607:修改了一下

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
+//https://service-azbaxpok-1251140691.sg.apigw.tencentcs.com/release
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://service-azbaxpok-1251140691.sg.apigw.tencentcs.com/release';
+  process.env.OPENAI_API_HOST || 'https://goasia.apps.mapking.com/chat';
